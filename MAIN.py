4fce276e2020cd94d0283e13f5af2d5b5f133f12
MCarlos7 <cmariscal102@gmail.com>
2025-10-12 16:36:38 -0600

    Update last update date comment XD
    Changed the LAST UPDATE comment to reflect the new date (12/10/2025) XD

--- MAIN.py
+++ MAIN.py
@@ -2,7 +2,7 @@
 # Carlos Alberto Mariscal Romo CUCEI
 # My Repository: https://github.com/MCarlos7/MIN_COMPILER_PY-CPP.git
 
-#LAST UPDATE: 2024-06-12 ADDED COMPATIBILITY WITH C++-LIKE SYNTAX AND IF-ELSE, SWITCH-CASE
+#LAST UPDATE: 12/10/2025 ADDED COMPATIBILITY WITH C++-LIKE SYNTAX AND IF-ELSE, SWITCH-CASE
 from GUI import App
 
 if __name__ == "__main__":
